1c8d9784b320b8df4afc856a7f334218f0625137
Harry McLean <harry.a.mclean@outlook.com>
2022-02-08 21:23:25 +1300

    Initial commit

---
+++ Shared/footstepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  footstepsApp.swift
+//  Shared
+//
+//  Created by Harry McLean on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct footstepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
