f43ed369dabd54645ac8b80528654a4387e8e7af
Harry McLean <harry.a.mclean@outlook.com>
2022-05-12 22:01:40 +1200

    Added DetailViews and transitions between screens

---
+++ Shared/ScrumsView.swift
@@ -0,0 +1,38 @@
+//
+//  ScrumsView.swift
+//  footsteps (iOS)
+//
+//  Created by Harry McLean on 12/02/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColour)
+            }   
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button(action: {}) {
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
+    }
+}
